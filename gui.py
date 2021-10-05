6b2150f097d28803ba8e600d839a1a67ab08efb0
booyah976 <booyah976@gmail.com>
2021-10-06 00:37:15 +0530

    Initial Commit!

---
+++ gui.py
@@ -0,0 +1,101 @@
+import os
+from main import get_stats, compile_stats
+import subprocess as sp
+import threading
+
+gui = True
+try:
+    import PySimpleGUI as sg
+except ModuleNotFoundError:
+    print("PySimpleGUI not found, resuming without GUI")
+    gui = False
+
+
+font = ("HUN", 36)
+current_dir = os.path.dirname(os.path.realpath(__file__))
+
+
+username = "booyah"  ## Must enter TETR.IO username here and not in capitals. Eg.) username = "booyah"
+
+
+def csv():
+    programName = "notepad.exe"
+    fileName = current_dir + r"\data.csv"
+    sp.Popen([programName, fileName])
+
+
+def open_tetrio():
+    os.system(r"")
+
+
+def open_project():
+    sp.Popen(r'explorer /select,"' + current_dir + '"')
+    print(r'explorer /select,"' + current_dir + '"')
+
+
+if gui:
+    layout = [
+        [
+            sg.Text("Tetra League Statistics", font=font),
+            sg.Text("          "),
+            sg.Button("RUN", button_color=(sg.GREENS[0]), size=(12, 2)),
+        ],
+        [sg.FileBrowse(button_text="Browse")],
+        [
+            sg.Button("OPEN PROJECT"),
+            sg.Button("OPEN CSV"),
+            sg.Text("                         "),
+            sg.Button("EXIT", button_color=("red"), size=(5, 2)),
+        ],
+        [sg.Text("PRESS RUN TO RUN THE PROGRAM", text_color="black", key="_TBOX_")],
+    ]
+
+    # Create the window
+    window = sg.Window("TL Stats", layout, margins=(100, 70), font=("HUN", 16))
+
+    # Create an event loop
+    while True:
+        event, values = window.read()
+
+        if event == "EXIT" or event == sg.WIN_CLOSED:
+            break
+
+        if event == "RUN":
+            t1 = threading.Thread(target=compile_stats, args=[username])
+            t1.start()
+            window.Element("_TBOX_").Update("RUN SUCCESSFULLY!")
+
+        if event == "OPEN CSV":
+            t2 = threading.Thread(target=csv)
+            t2.start()
+            window.Element("_TBOX_").Update("OPENED CSV!")
+
+        if event == "OPEN TETR.IO":
+            t3 = threading.Thread(target=open_tetrio)
+            t3.start()
+            window.Element("_TBOX_").Update("OPENED TETR.IO")
+
+        if event == "OPEN PROJECT":
+            t4 = threading.Thread(target=open_project)
+            t4.start()
+            window.Element("_TBOX_").Update("OPENED PROJECT FOLDER")
+
+    window.close()
+else:
+    print("Continuing without PySimpleGUI")
+    text = "Type R to run the program \nType C to open CSV file \nType P to open project folder \nType Q to quit"
+    print(text)
+    run = True
+    while run:
+        action = input("tl-stats>>")
+        if action == "R":
+            compile_stats(username)
+            print("Completed")
+        elif action == "C":
+            csv()
+        elif action == "P":
+            open_project()
+        elif action == "Q":
+            run = False
+        else:
+            print(text)
---
+++ main.py
@@ -0,0 +1,117 @@
+import json
+import os
+import requests
+import csv
+
+
+def get_stats(player):
+    # average apm
+    apm = round(player["points"]["secondary"], 2)
+    pps = round(player["points"]["tertiary"], 2)
+    vs = round(player["points"]["extra"]["vs"], 2)
+
+    a_apm = []
+    a_vs = []
+    a_pps = []
+
+    # individual games apm
+    for item in player["points"]["secondaryAvgTracking"]:
+        item = round(item, 2)
+        a_apm.append(item)
+    for item in player["points"]["tertiaryAvgTracking"]:
+        item = round(item, 2)
+        a_vs.append(item)
+    for item in player["points"]["extraAvgTracking"]["aggregatestats___vsscore"]:
+        item = round(item, 2)
+        a_pps.append(item)
+
+    return (apm, pps, vs, a_apm, a_pps, a_vs)
+
+
+def compile_stats(username):
+    request_rating = "N/A"
+    response = requests.get(f"https://ch.tetr.io/api/users/{username}")
+
+    try:
+        usable_response = response.json()
+    except ValueError:
+        print("Empty response, did you type in the correct username in gui.py?")
+        usable_response = {"success": False}
+        request_rating = "N/A"
+
+    if usable_response["success"]:
+        # request_ts = usable_response["data"]["user"]["ts"]
+        request_rating = usable_response["data"]["user"]["league"]["rating"]
+        # standing = usable_response["data"]["user"]["league"]["standing"]
+        # gametime = usable_response["data"]
+
+    entries = os.listdir("replays/")
+    for replay_file in entries:
+        f = open("replays/" + str(replay_file))
+        usable_f = json.load(f)
+
+        timestamp = usable_f["ts"]
+
+        try:
+            gametype = usable_f["gametype"]
+        except KeyError:
+            gametype = "custom"
+
+        endcontext = usable_f["endcontext"]
+
+        if endcontext[0]["user"]["username"] == f"{username}":
+            user_index = 0
+            opp_index = 1
+        else:
+            user_index = 1
+            opp_index = 0
+
+        player1 = endcontext[user_index]
+        player2 = endcontext[opp_index]
+
+        score1 = player1["wins"]
+        score2 = player2["wins"]
+
+        opponent = player2["user"]["username"]
+
+        apm1, pps1, vs1, a_apm1, a_pps1, a_vs1 = get_stats(player1)
+        apm2, pps2, vs2, a_apm2, a_pps2, a_vs2 = get_stats(player2)
+
+        exists = os.path.isfile("data.csv")
+
+        with open("data.csv", "a", newline="") as f:
+            writer = csv.writer(f)
+            header = [
+                "Timestamp",
+                "Opponent",
+                "Score1",
+                "Score2",
+                "Apm1",
+                "Pps1",
+                "Vs1",
+                "Apm2",
+                "Pps2",
+                "Vs2",
+                "rating",
+                "gametype",
+            ]
+            csv_data = [
+                timestamp,
+                opponent,
+                score1,
+                score2,
+                apm1,
+                pps1,
+                vs1,
+                apm2,
+                pps2,
+                vs2,
+                request_rating,
+                gametype,
+            ]
+            if not exists:
+                writer.writerow(header)
+            writer.writerow(csv_data)
+
+        os.remove("replays/" + str(replay_file))
+    return 0
